5666779e8271c2dc355e11afbda0dc3ba9fcec4c
unknown <araujoantoniojsd@gmail.com>
2026-04-04 21:44:37 -0300

    Adicionando a biblioteca .env para variáveis globais

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,24 +1,20 @@
 const request = require('supertest');
 const { expect } = require('chai');
-require('dotenv').config()
+require('dotenv').config();
+const { obterToken } = require('../helpers/autenticacao');
 
 describe('Transferência', () => {
+
     describe('POST /transferencia', () => {
-        it('Deve retornar sucesso com 201 quando o valor for >= 10,00', async () => {
+
+        it('Deve retornar sucesso com 201 quando o valor for igual ou acima 10,00', async () => {
             
-            const respostaLogin = await request(process.env.BASE_URL)
-                    .post('/transferencias')
-                    .set('Content-Type', 'application/json')
-                    .send({
-                        username: 'julio.lima',
-                        senha: '123456'
-                    });
-            const token = respostaLogin.budy.token    
+            const token = await obterToken('julio.lima', '123456');
 
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencia')
                 .set('Content-Type', 'application/json')
-                .set('Autorization',`Bearer ${token}`)
+                .set('Authorization', `Bearer ${token}`)
                 .send({
                     contaOrigem: 1,
                     contaDestino: 2,
@@ -32,19 +28,12 @@ describe('Transferência', () => {
 
         it('Deve retornar falha com 422 quando o valor for abaixo de 10,00', async () => {
            
-            const respostaLogin = await request('http//localhost:3000')
-                    .post('/login')
-                    .set('Content-Type', 'application/json')
-                    .send({
-                        username: 'julio.lima',
-                        senha: '123456'
-                    });
-            const token = respostaLogin.budy.token    
+            const token = await obterToken('julio.lima', '123456');
 
-            const resposta = await request('http//localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencia')
                 .set('Content-Type', 'application/json')
-                .set('Autorization',`Bearer ${token}`)
+                .set('Authorization', `Bearer ${token}`)
                 .send({
                     contaOrigem: 1,
                     contaDestino: 2,
@@ -52,11 +41,11 @@ describe('Transferência', () => {
                     token: ""
                 });
 
-
             expect(resposta.status).to.equal(422);
-            console.log(resposta.budy)
+
         });
 
     });
 
 });
+
